e9295a9d3d853602ea012a86c9c073370080ed6f
lab2139 <lab2139@uncw.edu>
2017-12-02 19:00:45 -0500

    functionality to add movies to database

--- AddMovie.java
+++ AddMovie.java
@@ -3,6 +3,8 @@ import java.awt.Cursor;
 import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.Image;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.*;
 import javax.swing.border.EmptyBorder;
@@ -15,68 +17,50 @@ import javax.swing.border.LineBorder;
  * @author laurenblatchford
  *
  */
-public class AddMovie {
+public class AddCustomer {
 	public static JPanel everythingPanel;
-	private JPanel addMoviePanel;
-	private JPanel addMovieLabelPanel;
-	private JPanel urlPanel;
-	private JPanel titlePanel;
-	private JPanel directorPanel;
-	private JPanel genrePanel;
-	private JPanel actorPanel;
-	private JPanel ratingPanel;
-	private JPanel quantityPanel;
-	private JPanel addMovieButtonPanel;
-	private JLabel addMovieLabel;
-	private JLabel urlLabel;
-	private JLabel titleLabel;
-	private JLabel directorLabel;
-	private JLabel genreLabel;
-	private JLabel actorLabel;
-	private JLabel ratingLabel;
-	private JLabel quantityLabel;
-	private JTextField enterUrl;
-	private JTextField enterTitle;
-	private JTextField enterDirector;
-	private JTextField enterGenre;
-	private JTextField enterActor;
-	private JTextField enterRating;
-	private JTextField enterQuantity;
-	private JButton goAddMovie;
+	//private JLabel addCustomerPanel;
+	private JPanel addCustomerLabelPanel;
+	private JPanel firstNamePanel;
+	private JPanel lastNamePanel;
+	private JPanel emailPanel;
+	private JPanel phonePanel;
+	private JPanel addCustomerButtonPanel;
+	private JLabel addCustomerLabel;
+	private JLabel firstNameLabel;
+	private JLabel lastNameLabel;
+	private JLabel emailLabel;
+	private JLabel phoneLabel;
+	private JTextField enterFirstName;
+	private JTextField enterLastName;
+	private JTextField enterEmail;
+	private JTextField enterPhone;
+	private JButton goAddCustomer;
 	private Font font;
 	private Font largeFont;
 	
-	public AddMovie(){
+	public AddCustomer(){
 		//Creates labels for search movies and customers
 		//Sets the font to Calisto MT, white
 		font = new Font("Calisto MT", Font.ITALIC, 12);
 		largeFont = new Font("Calisto MT", Font.BOLD, 16);
 		
 		//Creates Labels for adding movies including: URL, Title, director, Genre, Actor, Rating, Quantity
-		addMovieLabel = new JLabel("Add Movie: ");
-		addMovieLabel.setForeground(Color.white);
-		addMovieLabel.setFont(largeFont);
-		urlLabel = new JLabel("URL: ");
-		urlLabel.setForeground(Color.white);
-		urlLabel.setFont(font);
-		titleLabel = new JLabel("Title: ");
-		titleLabel.setForeground(Color.white);
-		titleLabel.setFont(font);
-		directorLabel = new JLabel("Director: ");
-		directorLabel.setForeground(Color.white);
-		directorLabel.setFont(font);
-		genreLabel = new JLabel("Genre: ");
-		genreLabel.setForeground(Color.white);
-		genreLabel.setFont(font);
-		actorLabel = new JLabel("Actor: ");
-		actorLabel.setForeground(Color.white);
-		actorLabel.setFont(font);
-		ratingLabel = new JLabel("Rating: ");
-		ratingLabel.setForeground(Color.white);
-		ratingLabel.setFont(font);
-		quantityLabel = new JLabel("Quantity: ");
-		quantityLabel.setForeground(Color.white);
-		quantityLabel.setFont(font);
+		addCustomerLabel = new JLabel("Add Customer: ");
+		addCustomerLabel.setForeground(Color.white);
+		addCustomerLabel.setFont(largeFont);
+		firstNameLabel = new JLabel("First Name: ");
+		firstNameLabel.setForeground(Color.white);
+		firstNameLabel.setFont(font);
+		lastNameLabel = new JLabel("Last Name: ");
+		lastNameLabel.setForeground(Color.white);
+		lastNameLabel.setFont(font);
+		emailLabel = new JLabel("Email: ");
+		emailLabel.setForeground(Color.white);
+		emailLabel.setFont(font);
+		phoneLabel = new JLabel("Phone Number (xxx-xxx-xxxx): ");
+		phoneLabel.setForeground(Color.white);
+		phoneLabel.setFont(font);
 		
 		//Go button images
 		ImageIcon go1big = new ImageIcon("Go1.jpg");
@@ -90,78 +74,72 @@ public class AddMovie {
 		
 		
 		//Go button -> Adds movie to database
-		goAddMovie = new JButton();
-		goAddMovie.setIcon(go1);
-		goAddMovie.setBorder(null);
-		goAddMovie.setRolloverIcon(go2);
-		goAddMovie.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+		goAddCustomer = new JButton();
+		goAddCustomer.setIcon(go1);
+		goAddCustomer.setBorder(null);
+		goAddCustomer.setRolloverIcon(go2);
+		goAddCustomer.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+		goAddCustomer.addActionListener(new ActionListener(){
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+				String first_name = enterFirstName.getText();
+				String last_name = enterLastName.getText();
+				String email = enterEmail.getText();
+				String phone = enterPhone.getText();
+				RQ.AddUser(first_name, last_name, phone, email);
+				String message = first_name + " is added to the database.";
+				JOptionPane.showMessageDialog(UI.frame, message);
+			}
+			
+		});
 		
 		//TextFields
-		enterUrl = new JTextField(25);
-		enterTitle = new JTextField(25);
-		enterDirector = new JTextField(25);
-		enterGenre = new JTextField(25);
-		enterActor = new JTextField(25);
-		enterRating = new JTextField(25);
-		enterQuantity = new JTextField(25);
+		enterFirstName = new JTextField(25);
+		enterLastName = new JTextField(25);
+		enterEmail = new JTextField(25);
+		enterPhone = new JTextField(25);
 		
 		//Create Panels
-		addMoviePanel = new JPanel();
-		addMoviePanel.setLayout(new BoxLayout(addMoviePanel, BoxLayout.Y_AXIS));
-		addMoviePanel.setBackground(Color.BLACK);
-		addMoviePanel.setBorder(new LineBorder(Color.white, 3));
-		addMovieLabelPanel = new JPanel();
-		addMovieLabelPanel.setBackground(Color.BLACK);
-		urlPanel = new JPanel();
-		urlPanel.setBackground(Color.BLACK);
-		titlePanel = new JPanel();
-		titlePanel.setBackground(Color.black);
-		directorPanel = new JPanel();
-		directorPanel.setBackground(Color.BLACK);
-		genrePanel = new JPanel();
-		genrePanel.setBackground(Color.black);
-		actorPanel = new JPanel();
-		actorPanel.setBackground(Color.black);
-		ratingPanel = new JPanel();
-		ratingPanel.setBackground(Color.black);
-		quantityPanel = new JPanel();
-		quantityPanel.setBackground(Color.BLACK);
-		addMovieButtonPanel = new JPanel();
-		addMovieButtonPanel.setBackground(Color.BLACK);
+		//addCustomerPanel.setLayout(new BoxLayout(addCustomerPanel, BoxLayout.Y_AXIS));
+		//addCustomerPanel.setBackground(Color.BLACK);
+		//addCustomerPanel.setBorder(new LineBorder(Color.white, 3));
+		addCustomerLabelPanel = new JPanel();
+		addCustomerLabelPanel.setBackground(Color.BLACK);
+		firstNamePanel = new JPanel();
+		firstNamePanel.setBackground(Color.BLACK);
+		lastNamePanel = new JPanel();
+		lastNamePanel.setBackground(Color.black);
+		emailPanel = new JPanel();
+		emailPanel.setBackground(Color.BLACK);
+		phonePanel = new JPanel();
+		phonePanel.setBackground(Color.black);
+		addCustomerButtonPanel = new JPanel();
+		addCustomerButtonPanel.setBackground(Color.BLACK);
 		
 		//Add labels, textfields, and buttons to respective panels
-		addMovieLabelPanel.add(addMovieLabel);
-		urlPanel.add(urlLabel);
-		urlPanel.add(enterUrl);
-		titlePanel.add(titleLabel);
-		titlePanel.add(enterTitle);
-		directorPanel.add(directorLabel);
-		directorPanel.add(enterDirector);
-		genrePanel.add(genreLabel);
-		genrePanel.add(enterGenre);
-		actorPanel.add(actorLabel);
-		actorPanel.add(enterActor);
-		ratingPanel.add(ratingLabel);
-		ratingPanel.add(enterRating);
-		quantityPanel.add(quantityLabel);
-		quantityPanel.add(enterQuantity);
-		addMovieButtonPanel.add(goAddMovie);
-		
-		//Add panels to encompassing panel
-		addMoviePanel.add(addMovieLabelPanel);
-		addMoviePanel.add(urlPanel);
-		addMoviePanel.add(titlePanel);
-		addMoviePanel.add(directorPanel);
-		addMoviePanel.add(genrePanel);
-		addMoviePanel.add(ratingPanel);
-		addMoviePanel.add(quantityPanel);
-		addMoviePanel.add(addMovieButtonPanel);
+		addCustomerLabelPanel.add(addCustomerLabel);
+		firstNamePanel.add(firstNameLabel);
+		firstNamePanel.add(enterFirstName);
+		lastNamePanel.add(lastNameLabel);
+		lastNamePanel.add(enterLastName);
+		emailPanel.add(emailLabel);
+		emailPanel.add(enterEmail);
+		phonePanel.add(phoneLabel);
+		phonePanel.add(enterPhone);
+		addCustomerButtonPanel.add(goAddCustomer);
 		
 		//Adds everything to one panel to be added to the UI
 		everythingPanel = new JPanel();
 		everythingPanel.setLayout(new BoxLayout(everythingPanel, BoxLayout.Y_AXIS));
 		everythingPanel.setBackground(Color.BLACK);
-		everythingPanel.add(addMoviePanel);
+		everythingPanel.add(addCustomerLabelPanel);
+		everythingPanel.add(firstNamePanel);
+		everythingPanel.add(lastNamePanel);
+		everythingPanel.add(emailPanel);
+		everythingPanel.add(phonePanel);
+		everythingPanel.add(addCustomerButtonPanel);
 	}
 	
 	/**
